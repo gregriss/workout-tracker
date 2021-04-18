1a2e192c1e0c16e451f98fc3ad1ee844f8e25ae8
Greg Riss <gregriss23@gmail.com>
2021-04-17 23:51:30 -0400

    fixing MONGODB config and model routes

--- models/Exercise.js
+++ models/Exercise.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-const ExerciseSchema = new Schema({
-    type: String,
-    name: String,
-    distance: Number,
-    duration: Number,
-    weight: Number,
-    reps: Number,
-    sets: Number
-});
+// const ExerciseSchema = new Schema({
+//     type: String,
+//     name: String,
+//     distance: Number,
+//     duration: Number,
+//     weight: Number,
+//     reps: Number,
+//     sets: Number
+// });
 
 // const Exercise = mongoose.model('Exercise', ExerciseSchema);
 
--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 let mongoose = require("mongoose");
-let db = require("../models/modelObjects");
+let db = require("../models/Workout.js");
 
 // if(process.env.MONGO_ATLAS.length > 0){
 //   mongoose.connect(process.env.MONGO_ATLAS, {
